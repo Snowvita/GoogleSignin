a1b6caa572143c54baaa859f2cb7b606be5bea51
Snowvita <138800453+Snowvita@users.noreply.github.com>
2023-08-02 21:55:38 +0530

    Add files via upload

---
+++ SampleAuthentication/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { SocialAuthService  } from '@abacritt/angularx-social-login';
+import { SocialUser } from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  user: SocialUser | null; 
+
+  constructor(private authService: SocialAuthService) 
+  { 
+  this.user = null;
+  this.authService.authState.subscribe((user: SocialUser) => {
+    console.log(user);
+    this.user = user;
+  });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then((x: any) => console.log(x));
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }  
+} 
